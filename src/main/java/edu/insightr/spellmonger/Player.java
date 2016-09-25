844c61187c814473b1266584461be91471730d72
ValentinDuph <v.duphenieux@gmail.com>
2016-09-25 18:48:52 +0200

    Ajout de la classe Player

---
+++ src/main/java/edu/insightr/spellmonger/Player.java
@@ -0,0 +1,63 @@
+package edu.insightr.spellmonger;
+
+import org.apache.log4j.Logger;
+import sun.font.CreatedFontTracker;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by ValentinDuph on 25/09/2016.
+ */
+public class Player {
+    private static final Logger logger = Logger.getLogger(SpellmongerApp.class);
+
+    private String name;
+    private int lifePoint;
+    private List<Creature> playerCreature;
+
+    public Player(String name)
+    {
+        this.name = name;
+        lifePoint = 20;
+    }
+
+    public void DrawCard(Player currentPlayer, Player opponent, List<Card> cardPool, List<Card> displayCard)
+    {
+        Card currentCard = cardPool.get(0); //Recupere la premiere carte de la pile
+        cardPool.remove(0);
+        displayCard.add(currentCard);
+
+        logger.info(currentPlayer.toString() + " draw a "+ currentCard);
+
+        if (currentCard instanceof Creature) {
+            currentPlayer.playerCreature.add((Creature) currentCard);
+            opponent.lifePoint = opponent.GetLifePoint() - currentPlayer.playerCreature.size();
+            logger.info("The " + currentPlayer.playerCreature.size() + " creatures of " + currentPlayer.toString() + " attack and deal " + currentPlayer.playerCreature.size() + " damages to its opponent");
+        }
+        if (currentCard instanceof Ritual) {
+
+            opponent.lifePoint = opponent.GetLifePoint() - currentPlayer.playerCreature.size() - 3;
+            logger.info("The " + currentPlayer.playerCreature.size() + " creatures of " + currentPlayer.toString() + " attack and deal " + currentPlayer.playerCreature.size() + " damages to its opponent");
+            logger.info(currentPlayer.toString() + " cast a ritual that deals 3 damages to " + opponent.toString());
+        }
+    }
+
+    public boolean isDead()
+    {
+        if(lifePoint <= 0){ return true; }
+        else{ return false; }
+    }
+
+    public int GetLifePoint()
+    {
+        return lifePoint;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Player " + name + "(" + GetLifePoint() + "pv) ";
+    }
+
+}
