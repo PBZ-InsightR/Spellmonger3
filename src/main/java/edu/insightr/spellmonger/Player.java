dac6a3656d36a8ef969f181139d25472dee2bd12
NicolasHervelin <Hervelin.N@gmail.com>
2016-09-26 11:32:58 +0200

    Modif class player: Ajout GetPlayerCreature() Ajout AddCreature() Suppression DrawCard()

--- src/main/java/edu/insightr/spellmonger/Player.java
+++ src/main/java/edu/insightr/spellmonger/Player.java
@@ -3,6 +3,8 @@ package edu.insightr.spellmonger;
 import org.apache.log4j.Logger;
 import sun.font.CreatedFontTracker;
 
+import java.util.List;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,38 +17,23 @@ public class Player {
     private int lifePoint;
     private ArrayList<Creature> playerCreature;
 
-    public Player(String name)
-    {
+    public Player(String name) {
         this.playerCreature = new ArrayList<Creature>();
         this.name = name;
         lifePoint = 20; // Comment acceder à cette variable
-    }
-
-    public void DrawCard(Player currentPlayer, Player opponent, List<Card> cardPool, List<Card> displayCard)
-    {
-        Card currentCard = cardPool.get(0); //Recupere la premiere carte de la pile
-        cardPool.remove(0);
-        displayCard.add(currentCard);
-
-        logger.info(currentPlayer.toString() + " draw a "+ currentCard);
-
-        if (currentCard instanceof Creature) {
-            currentPlayer.playerCreature.add((Creature) currentCard);
-            opponent.lifePoint = opponent.GetLifePoint() - currentPlayer.playerCreature.size();
-            logger.info("The " + currentPlayer.playerCreature.size() + " creatures of " + currentPlayer.toString() + " attack and deal " + currentPlayer.playerCreature.size() + " damages to its opponent");
-        }
-        if (currentCard instanceof Ritual) {
-
-            opponent.lifePoint = opponent.GetLifePoint() - currentPlayer.playerCreature.size() - 3;
-            logger.info("The " + currentPlayer.playerCreature.size() + " creatures of " + currentPlayer.toString() + " attack and deal " + currentPlayer.playerCreature.size() + " damages to its opponent");
-            logger.info(currentPlayer.toString() + " cast a ritual that deals 3 damages to " + opponent.toString());
-        }
     }
 
     public boolean isDead()
     {
         if(lifePoint <= 0){ return true; }
         else{ return false; }
+    }
+
+    public ArrayList<Creature> GetPlayerCreature(){ return playerCreature;}
+
+    public void AddCreature(Creature creature)
+    {
+        playerCreature.add(creature);
     }
 
     public int GetLifePoint()
