152ff710f653ce06a7cfe7bf74c8d896e62af66b
Abdellah <sabry.abdellah@gmail.com>
2016-10-05 16:22:17 +0200

    Toutes les classes test crées dans le projet.

--- src/test/java/edu/insightr/spellmonger/DeckTest.java
+++ src/test/java/edu/insightr/spellmonger/DeckTest.java
@@ -1,6 +1,9 @@
 package edu.insightr.spellmonger;
 
 import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
@@ -9,7 +12,44 @@ import static org.junit.Assert.*;
  */
 public class DeckTest {
 
-    Deck d=new Deck("Test");
+    @Test
+    public void testSize() throws Exception {
+        Deck d = new Deck("Test");
+        Assert.assertEquals(70.0, d.size(), 0.1);
+
+        Deck d2 = new Deck();
+        Assert.assertEquals(0.0, d2.size(), 0.1);
+
+        ArrayList<Card> temp= new ArrayList<>();
+        Bear b=new Bear();
+        temp.add(b);
+        temp.add(new Bear());
+        temp.add(new Bear());
+        temp.add(new Curse());
+        temp.add(new Wolf());
+        Deck d3=new Deck(temp);
+        Assert.assertEquals(temp.size(), d3.size());
+    }
+
+    @Test
+    public void testGetCardPool() {
+        Deck d=new Deck("Test");
+        Assert.assertEquals(d.size(),d.getCardPool().size());
+    }
+
+    @Test
+    public void testGet(){
+        ArrayList<Card> temp= new ArrayList<>();
+        Bear b=new Bear();
+        temp.add(b);
+        temp.add(new Bear());
+        temp.add(new Bear());
+        temp.add(new Curse());
+        temp.add(new Wolf());
+        Deck d3=new Deck(temp);
+        Assert.assertEquals(b,d3.get(0));
+    }
+
 
 
 
