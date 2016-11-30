f1da62c619350b3cbb87b6941ec0697dfbea96ce
badre <badre.fo@gmail.com>
2016-11-30 09:21:52 +0100

    Correcteur

--- src/test/java/edu/insightr/spellmonger/ControllerTest.java
+++ src/test/java/edu/insightr/spellmonger/ControllerTest.java
@@ -7,13 +7,14 @@ import org.junit.Test;
 /**
  * Created by badre on 19/10/2016.
  */
+
 public class ControllerTest {
     @Test
     public void draw1Test() throws Exception{
         ControllerPlay c = new ControllerPlay();
         int nb_hand = c.getPlayer1().getHand().size();
         c.draw_player_1();
-        Assert.assertEquals(nb_hand,c.getPlayer1().getHand().size(),0.0);
+        Assert.assertEquals(nb_hand+1,c.getPlayer1().getHand().size(),0.0);
 
     }
     @Test
