608cb3909fbe68021431dbabdfb586d7435c65aa
Abdellah <sabry.abdellah@gmail.com>
2016-12-13 18:22:36 +0100

    2 level IA ajoutés

--- src/main/java/edu/insightr/Controller/ControlledScreen.java
+++ src/main/java/edu/insightr/Controller/ControlledScreen.java
@@ -3,5 +3,5 @@ package edu.insightr.Controller;
 
 interface ControlledScreen {
     //This method will allow the injection of the Parent ScreenPane
-    public void setScreenParent(ScreensController screenPage);
+    void setScreenParent(ScreensController screenPage);
 }
